ef4b571cba0eed009cc27e06b99ded21162c83af
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2024-03-21 14:28:05 -0400

    Added some buttons.

--- src/App.java
+++ src/App.java
@@ -6,12 +6,50 @@ public class App {
     public static final String TITLE = "Editor";
 
     public static final int WIDTH = 1600, HEIGHT = 900;
-    public static void main(String[] args) {
+
+    public void addMenuItems(JMenuBar bar) {
+
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem save = new JMenuItem("Save");
+        JMenuItem open = new JMenuItem("Open");
+
+        bar.add(fileMenu);
+        fileMenu.add(save);
+        fileMenu.add(open);
+
+        open.addActionListener(e -> {
+
+            JFileChooser chooser = new JFileChooser();
+            JFrame frame = new JFrame("Select File");
+
+            chooser.showOpenDialog(frame);
+            System.out.println(chooser.getSelectedFile());
+
+        });
+
+        save.addActionListener(e -> {
+
+        });
+
+    }
+
+    private JTabbedPane editors;
+    public App() {
+        editors = new JTabbedPane();
+    }
+
+    public void init() {
 
         JFrame frame = new JFrame(TITLE);
         JPanel panel = new JPanel();
         Toolkit tk = Toolkit.getDefaultToolkit();
         Dimension screenSize = tk.getScreenSize();
+
+        JMenuBar topMenu = new JMenuBar();
+
+        addMenuItems(topMenu);
+        panel.add(topMenu);
+        panel.add(editors);
 
         panel.setSize(WIDTH, HEIGHT);
         panel.setPreferredSize(panel.getSize());
@@ -22,5 +60,9 @@ public class App {
         frame.setLocation((screenSize.width - frame.getWidth()) / 2, 0);
         frame.setVisible(true);
 
+    }
+
+    public static void main(String[] args) {
+        new App().init();
     }
 }
