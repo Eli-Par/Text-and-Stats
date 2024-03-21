a55e362d639e74532c5231a5bb07abf35d2eccaf
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2024-03-21 01:04:28 -0400

    JFrame is shown.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static final String TITLE = "Editor";
+
+    public static final int WIDTH = 1600, HEIGHT = 900;
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame(TITLE);
+        JPanel panel = new JPanel();
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+
+        panel.setSize(WIDTH, HEIGHT);
+        panel.setPreferredSize(panel.getSize());
+        frame.add(panel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocation((screenSize.width - frame.getWidth()) / 2, 0);
+        frame.setVisible(true);
+
     }
 }
