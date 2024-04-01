1887a33ac2777cd77c4f39ecbcb8d3bdb25d1914
MikeGibb7 <119349139+MikeGibb7@users.noreply.github.com>
2024-04-01 17:43:29 -0400

    Fixed Fonts that were not showing up

--- src/FontListCellRenderer.java
+++ src/FontListCellRenderer.java
@@ -6,10 +6,18 @@ public class FontListCellRenderer extends DefaultListCellRenderer {
     public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
         JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
+        Font defaultFont = new Font(Font.SANS_SERIF, Font.PLAIN, 12); // Default fallback font
+
+
         if (value instanceof String && !value.equals("---")) {
             String fontFamily = (String) value;
             Font font = new Font(fontFamily, Font.PLAIN, 12); 
-            label.setFont(font);
+            
+            if (font.canDisplayUpTo(label.getText()) == -1) {
+                label.setFont(font); 
+            } else {
+                label.setFont(defaultFont); 
+            }
         }
 
         return label;
